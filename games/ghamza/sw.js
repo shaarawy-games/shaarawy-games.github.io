173b1f663bb8b4a26222a296d6b5a4fa300b69b0
shaarawy-games <oeie9e8nbb@gmail.com>
2026-09-04 16:28:33 +0300

    Add files via upload

---
+++ games/ghamza/sw.js
@@ -0,0 +1,5 @@
+const CACHE='ghamza-v1';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(saved=>saved||fetch(event.request))));
